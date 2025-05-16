08345dd73c34dcac0f18cd4aa21289d50deb2b28
Brian <okellobrian18@gmail.com>
2025-05-16 21:22:34 +0300

    many to many

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,53 @@
+from datetime import datetime
+
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())   
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
 
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
+    
 
 class Contract:
-    pass
+    all = []  
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("author must be an Author instance")
+        if not isinstance(book, Book):
+            raise Exception("book must be a Book instance")
+        if not isinstance(date, str):
+            raise Exception("date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        filtered = [contract for contract in cls.all if contract.date == date]
+        return sorted(filtered, key=lambda c: c.date)
